e8cea4ba7a68ac5c8df031513e5fd2de472cdc6a
Royce <roycewhitaker@rocketmail.com>
2022-02-21 21:16:05 -0800

    Add files via upload

--- pc-butler.py
+++ pc-butler.py
@@ -1,2 +1,18 @@
 import sys
-print('version is', sys.version)
+print('Tested on 3.10.2. Your version: ', sys.version)
+print("*** **** ****")
+print("\n")
+
+def help():
+	print("*** HELP MENU ***")
+
+
+def main():
+	help()
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
