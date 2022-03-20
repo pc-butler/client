358a768fd92cb27453e5eabf4be7fa4d74b0c6a0
doorpot <57577649+doorpot@users.noreply.github.com>
2022-03-20 11:53:32 -0700

    split client functions into two scripts

--- host_discovery.py
+++ host_discovery.py
@@ -15,4 +15,4 @@ for query_ans in ans:
 
 if __name__ == "__main__":
     print(json.dumps(mac_table, sort_keys=True, indent=4, separators=(",", ": ")))
-    test_change = True
+    test_change = False
