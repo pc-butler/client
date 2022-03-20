5895c9e99d1bca8dd3a072a32263fc1164040959
doorpot <57577649+doorpot@users.noreply.github.com>
2022-03-20 11:28:53 -0700

    Update pc-butler.py
    developed functionality to generate mac address tables from host discovery

--- pc-butler.py
+++ pc-butler.py
@@ -1,11 +1,16 @@
-from scapy.layers.l2 import ARP, Ether, srp, Loopback
+from scapy.layers.l2 import ARP, Ether
+from scapy.sendrecv import srp
+import json
 
-import nmap
+ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst="192.168.2.0/24"), timeout=2, verbose=0)
+
+mac_table = {}
+
+for query_ans in ans:
+    for packet in query_ans:
+        device_mac = packet[Ether].src
+        device_ip = packet[ARP].psrc
+        mac_table[device_ip] = device_mac
 
 
-def main():
-    scanner = nmap.PortScanner()
-    scan_range = scanner.scan(hosts="192.168.2.101")
-
-if __name__ == "__main__":
-    main()
+print(json.dumps(mac_table, sort_keys=True, indent=4, separators=(",", ": ")))
