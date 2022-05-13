5abd79b10310e4ea2ea4603bfd4967cbad691c04
personal laptop <johncnwen@gmail.com>
2022-05-12 20:07:36 -0700

    clear_all py

--- clear_all.py
+++ clear_all.py
@@ -4,6 +4,7 @@ import requests
 def main():
     url = f"https://dashboard.pcbutler.net/api/delete_all"
     r = requests.get(url=url)
+    print("Database cleared.")
 
 
 if __name__ == "__main__":
